b60390973ebb31ca28cfd1849ee075568c1f5109
FoubertThomas <famille.foubert.thomas@gmail.com>
2021-01-02 19:11:40 +0100

    add vues etudiant

---
+++ src/components/Etudiant/AllEtudiant.vue
@@ -0,0 +1,40 @@
+
+<template>
+  <div id="app">
+    {{info}}
+  </div>
+</template>
+<script>
+export default {
+  name: 'Entreprises',
+  data () {
+    return {
+      info: null
+    }
+  },
+  mounted () {
+    JSON
+      .get('https://api.coindesk.com/v1/bpi/currentprice.json')
+      .then(response => (this.info = response))
+  }
+}
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/components/Etudiant/ModifEtudiant.vue
@@ -0,0 +1,35 @@
+
+<template>
+  <div id="app">
+    {{msg}}
+  </div>
+</template>
+<script>
+export default {
+  name: 'MODIFEtudiant',
+  data () {
+    return {
+      msg: 'Je veux modifier un etudiant'
+    }
+  }
+}
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
