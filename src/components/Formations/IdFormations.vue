044a01d84ca2cdb1ac1bb44b9f17f63789efea8c
FoubertThomas <famille.foubert.thomas@gmail.com>
2021-01-02 17:27:13 +0100

    add vues formations

---
+++ src/components/Formations/IdFormations.vue
@@ -0,0 +1,34 @@
+
+<template>
+  <div id="app">
+    {{msg}}
+  </div>
+</template>
+<script>
+export default {
+  name: 'IDFormations',
+  data () {
+    return {
+      msg: 'Je veux afficher une formation selon son id '
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
