60d84251574bc24b2d6aebe4c9bf07c07276ae0b
FoubertThomas <famille.foubert.thomas@gmail.com>
2021-01-02 15:15:55 +0100

    add api via param.js

---
+++ src/param/param.js
@@ -0,0 +1,26 @@
+const host = '.../api';
+
+export default{
+
+    addEntreprise : host + '/entreprise/add...',
+    siretEntreprise : host + '/entreprise/:siret...',
+    allEntreprise : host + '/all/entreprise...',
+
+    idEtudiant : host + '/etudiant/{id}...',
+    allEtudiant : host + '/all/etudiant...',
+    updateEtudiant : host + '/etudiant/{id}/modification...',
+
+    idFormation : host + '/formation/:id...',
+    allFormation : host + '/all/formation...',
+    depidDepFormations : host + '/all/formations/departement/:depid...',
+
+    idDepartement : host + '/departement/:id...',
+    allDepartement : host + '/all/departement...',
+
+    allMotsCles : host + '/all/motscle...',
+    depMotsCles : host + '/motscles/departements...',
+    formidFormationMotsCles : host + '/all/motscles/formation/:formid...',
+
+    addAnnonce : host + '/annonce/add...',
+    allAnnonce : host + '/all/annonce...',
+}
